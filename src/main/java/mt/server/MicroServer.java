68cd1b3ee55d07bb7f611c11a4a449ec30f6bc25
rmsss1-iscteiulpt <rmsss1@iscte-iul.pt>
2017-05-20 13:44:39 +0100

    Created MiniTrader-EU-1.1 Branch

--- src/main/java/mt/server/MicroServer.java
+++ src/main/java/mt/server/MicroServer.java
@@ -27,6 +27,8 @@ import mt.filter.AnalyticsFilter;
  * @author Group 78
  *
  */
+
+//EUROPEAN 
 public class MicroServer implements MicroTraderServer {
 	
 	public static void main(String[] args) {
